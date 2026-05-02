56b974997854389834251d8ba1a5aa4e8482b71b
Cornelius Pavlic <cornelius.pavlic@gmail.com>
2026-05-02 19:04:36 -0400

    Update for Foundry V12-V14 and fix module manifest

---
+++ README.md
@@ -0,0 +1,17 @@
+![Latest version](https://img.shields.io/github/v/tag/kid2407/DungeonScrawlImporter?label=Latest%20Version&sort=semver)
+![Latest Release Download Count](https://img.shields.io/github/downloads/kid2407/DungeonScrawlImporter/latest/module.zip?label=Downloads(latest))
+![Forge Installs](https://img.shields.io/badge/dynamic/json?label=Forge%20Installs&query=package.installs&suffix=%25&url=https%3A%2F%2Fforge-vtt.com%2Fapi%2Fbazaar%2Fpackage%2Fdungeon-scrawl-importer&colorB=4aa94a)
+
+# DungeonScrawlImporter
+
+Foundry module to allow importing a .ds file generated with Dungeon Scrawl to automatically generate walls.
+
+Simply export your Dungeon Scrawl file as an image and as a .ds file. Then create a scene in Foundry with the image and import the .ds file with thew button at the top of the scene sidebar.
+
+If you have a Dungeon Scrawl pro subscription, the importer now also allows to import the lights you place as light sources in Foundry, including all the attributes you could ever need.
+
+### Ko-Fi
+
+If you have read this far and want to show some more appreciation: I have a Ko-Fi, should you consider what I'm doing worth a little something. Of course I do not expect nor require anyone to use it, but I'll leave it here in case someone wants to give:
+
+[![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/H2H7ALZTI)
---
+++ scripts/DungeonScrawlImporterFormApplication.js
@@ -0,0 +1,300 @@
+export class DungeonScrawlImporterFormApplication extends FormApplication {
+    async _updateObject(event, formData) {
+        return Promise.resolve(undefined)
+    }
+
+    get title() {
+        return "DungeonScrawlImporter Importer"
+    }
+
+    static get defaultOptions() {
+        const options    = super.defaultOptions
+        options.id       = "dungeonScrawlImporter-import-dialogue"
+        options.template = `modules/dungeon-scrawl-importer/templates/import-dialogue.hbs`
+        options.width    = 500
+        options.height   = "auto"
+
+        return options
+    }
+
+    getData(options = {}) {
+        // noinspection JSValidateTypes
+        return {
+            "headline":    game.i18n.localize("DSI.import.headline"),
+            "chooseFile":  game.i18n.localize("DSI.import.chooseFile"),
+            "buttonText":  game.i18n.localize("DSI.import.import"),
+            "offsetXText": game.i18n.localize("DSI.import.offsetX"),
+            "offsetYText": game.i18n.localize("DSI.import.offsetY")
+        }
+    }
+
+    updateButtonStatus(text, disabled = false) {
+        $('#processDSFile').attr("disabled", disabled).html(text)
+    }
+
+    /**
+     * @param {Object} shapes
+     * @param {Array} doorIDs
+     * @param {Array} stairsIds
+     * @param {int} gridCellSize
+     * @param {int} offsetX
+     * @param {int} offsetY
+     * @return {{xOffset: number, yOffset: number}}
+     */
+    async parseShapes(shapes, doorIDs, stairsIds, gridCellSize, offsetX, offsetY) {
+        let wallData          = []
+        // noinspection JSValidateTypes
+        let currentScene      = game.scenes.current || canvas.scene
+        const sceneDimensions = currentScene.dimensions
+        const sceneCellSize   = currentScene.grid?.size || sceneDimensions.size
+        const gridFactor      = sceneCellSize / gridCellSize
+        for (let k in shapes) {
+            let isDoor = doorIDs.includes(k)
+            if (!shapes.hasOwnProperty(k)) continue
+            let element = shapes[k]
+            //If the element is a door the shape has to be reduced to a single segment to work properly as a foundry door.
+            if (isDoor) {
+                let doorVariant = 1
+                if (element.polylines.length > 0) doorVariant = 0
+
+                //For this variant the desired segment can be constructed by simply connecting the first point of the first polyline and the last point of the second polyline.
+                if (doorVariant === 0) {
+                    delete element.polygons
+                    element.polylines = [[element.polylines[0][0], element.polylines[1][1]]]
+                }
+                //For this variant the desired segment can be constructed by shifting one of the long edges of the rectangular polygon.
+                else if (doorVariant === 1) {
+                    //Determine the side lengths of the rectangular polygon.
+                    let doorLengths2 = []
+                    for (let i = 0; i < element.polygons[0][0].length; i++) {
+                        let doorLength2 = ((element.polygons[0][0][(i + 1) % (element.polygons[0][0].length)][0] - element.polygons[0][0][i][0]) ** 2 + (element.polygons[0][0][(i + 1) % (element.polygons[0][0].length)][1] - element.polygons[0][0][i][1]) ** 2)
+                        doorLengths2.push(doorLength2)
+                    }
+                    //Grab one of the long sides of the rectangular polygon.
+                    let longestSideIndex = doorLengths2.indexOf(Math.max(...doorLengths2))
+                    let longestSide      = [[element.polygons[0][0][longestSideIndex][0], element.polygons[0][0][longestSideIndex][1]], [element.polygons[0][0][(longestSideIndex + 1) % (element.polygons[0][0].length)][0], element.polygons[0][0][(longestSideIndex + 1) % (element.polygons[0][0].length)][1]]]
+
+                    //Grab the next side over relative to the long side. Since the shape is always a rectangle this must be a short side.
+                    let shortestSide         = [[element.polygons[0][0][(longestSideIndex + 1) % (element.polygons[0][0].length)][0], element.polygons[0][0][(longestSideIndex + 1) % (element.polygons[0][0].length)][1]], [element.polygons[0][0][(longestSideIndex + 2) % (element.polygons[0][0].length)][0], element.polygons[0][0][(longestSideIndex + 2) % (element.polygons[0][0].length)][1]]]
+                    //Determine the x,y offset, which is the difference between a point and a midway point along the short side.
+                    let shortestSideMidPoint = [(shortestSide[0][0] + shortestSide[1][0]) / 2, (shortestSide[0][1] + shortestSide[1][1]) / 2]
+                    let offset               = [shortestSide[0][0] - shortestSideMidPoint[0], shortestSide[0][1] - shortestSideMidPoint[1]]
+                    //Set the element's polyline to this single line segment, which is one of the long sides shifted by the previously determined offset along a short side.
+                    element.polylines[0]     = [[longestSide[0][0] - offset[0], longestSide[0][1] - offset[1]], [longestSide[1][0] - offset[0], longestSide[1][1] - offset[1]]]
+                    //Delete the polygon.
+                    delete element.polygons
+                }
+            }
+            // Ignore stairs
+            if (stairsIds.includes(k)) continue
+            if (element.hasOwnProperty('polylines')) {
+                element['polylines'].forEach((side) => {
+                    //Each sequential pair of points is a possible line, and
+                    //some of them won't have any length, so won't appear
+                    for (let i = 0; i + 1 < side.length; i = i + 1) {
+                        wallData.push({
+                            c:      [side[i][0] * gridFactor, side[i][1] * gridFactor, side[i + 1][0] * gridFactor, side[i + 1][1] * gridFactor],
+                            'door': isDoor,
+                        })
+                    }
+                })
+            }
+            if (element.hasOwnProperty('polygons')) {
+                element['polygons'].forEach(polygon => {
+                    polygon.forEach(list => {
+                        let firstPoint = []
+                        let lastPoint  = []
+                        list.forEach(side => {
+                            let coordinates = [
+                                side[0] * gridFactor,
+                                side[1] * gridFactor
+                            ]
+                            //Save the first point to connect with the last point later
+                            if (lastPoint.length === 0) {
+                                firstPoint = coordinates
+                            }
+                            if (lastPoint.length > 0) {
+                                wallData.push({
+                                    c:      [
+                                        lastPoint[0],
+                                        lastPoint[1],
+                                        coordinates[0],
+                                        coordinates[1],
+                                    ],
+                                    'door': isDoor
+                                })
+                            }
+                            lastPoint = coordinates
+                        })
+                        wallData.push({
+                            c:      [
+                                lastPoint[0],
+                                lastPoint[1],
+                                firstPoint[0],
+                                firstPoint[1],
+                            ],
+                            'door': isDoor
+                        })
+                    })
+                })
+            }
+
+        }
+
+        // Round to 3 decimals
+        wallData = wallData.map(function (element) {
+            element.c = element.c.map(value => Math.round(value * 100) / 100)
+            return element
+        })
+        wallData = wallData.filter((value) => {
+            return value.c[0] !== value.c[2] || value.c[1] !== value.c[3]
+        })
+
+        let uniqueKeys     = {}
+        let uniqueWallData = wallData.filter((value) => {
+            const key = value.c.join("_")
+            if (uniqueKeys.hasOwnProperty(key)) {
+                return false
+            }
+
+            uniqueKeys[key] = null
+            return true
+        })
+
+        let mostNegativeX = Math.min(...uniqueWallData.map(value => value.c[0]), ...uniqueWallData.map(value => value.c[2]))
+        let mostNegativeY = Math.min(...uniqueWallData.map(value => value.c[1]), ...uniqueWallData.map(value => value.c[3]))
+
+        let sceneX = sceneDimensions.sceneRect ? sceneDimensions.sceneRect.x : sceneDimensions.sceneX;
+        let sceneY = sceneDimensions.sceneRect ? sceneDimensions.sceneRect.y : sceneDimensions.sceneY;
+
+        let calculatedXOffset = sceneCellSize * offsetX + mostNegativeX * -1 + sceneX;
+        let calculatedYOffset = sceneCellSize * offsetY + mostNegativeY * -1 + sceneY;
+
+        uniqueWallData = uniqueWallData.map(value => {
+            value.c[0] = value.c[0] + calculatedXOffset
+            value.c[1] = value.c[1] + calculatedYOffset
+            value.c[2] = value.c[2] + calculatedXOffset
+            value.c[3] = value.c[3] + calculatedYOffset
+            return value
+        })
+        // noinspection JSUnresolvedFunction
+        await game.scenes.current.createEmbeddedDocuments("Wall", uniqueWallData)
+
+        return {xOffset: calculatedXOffset, yOffset: calculatedYOffset}
+    }
+
+    /**
+     * @param {Object} dataNodes
+     */
+    getDoorIDs(dataNodes) {
+        let doorIDs = []
+        for (let node in dataNodes) {
+            if (dataNodes[node].name === "Door geometry") doorIDs.push(dataNodes[node].geometryId)
+        }
+        return doorIDs
+    }
+
+    /**
+     * @param {Object} dataNodes
+     */
+    getStairIDs(dataNodes) {
+        let stairIDs = []
+        for (let node in dataNodes) {
+            if (dataNodes[node].name === "Stairs geometry") stairIDs.push(dataNodes[node].geometryId)
+        }
+        return stairIDs
+    }
+
+    /**
+     * @param {Object} dataNodes
+     */
+    getLights(dataNodes) {
+        let lightIDs = []
+        for (let node in dataNodes) {
+            if (dataNodes[node].type === "LIGHT") lightIDs.push(dataNodes[node])
+        }
+        return lightIDs
+    }
+
+    /**
+     * @param {Array} lightData
+     * @param {{xOffset: number, yOffset: number}} offsetData
+     * @param {number} gridCellSize
+     */
+    async processLights(lightData, offsetData, gridCellSize) {
+        let lights      = []
+        let data        = []
+        let lightType   = ''
+        let lightConfig = {}
+        let currentScene = game.scenes.current || canvas.scene;
+
+        for (let key in lightData) {
+            data        = lightData[key]
+            lightType   = data["variant"]
+            lightConfig = data.config
+
+            let light = {
+                x:      data.transform[4] * (currentScene.grid?.size / gridCellSize || currentScene.dimensions.size / gridCellSize) + offsetData.xOffset,
+                y:      data.transform[5] * (currentScene.grid?.size / gridCellSize || currentScene.dimensions.size / gridCellSize) + offsetData.yOffset,
+                config: {
+                    alpha:      data.alpha,
+                    bright:     lightConfig[lightType].radius,
+                    color:      "#" + lightConfig.shared.colour.colour.toString(16).padStart(6, "0"),
+                    dim:        lightConfig[lightType].radius,
+                    hidden:     !data.visible,
+                    luminosity: lightConfig[lightType].strength,
+                }
+            }
+            lights.push(light)
+        }
+
+        await currentScene.createEmbeddedDocuments("AmbientLight", lights)
+    }
+
+    /**
+     * @param {int} offsetX
+     * @param {int} offsetY
+     */
+    async loadFileContent(offsetX, offsetY) {
+        /** @var {File} */
+        let file   = document.getElementsByName("filePath")[0].files[0]
+        const self = this
+        if (file) {
+            let zip        = await JSZip.loadAsync(file)
+            let key        = Object.keys(zip.files)[0]
+            let targetFile = zip.files[key]
+            let jsonData   = await targetFile.async("string")
+            let data       = JSON.parse(jsonData)
+
+            let geometryData = data.data.geometry
+            let dataNodes    = data.state.document.nodes
+            let doorIDs      = self.getDoorIDs(dataNodes)
+            let stairIDs     = self.getStairIDs(dataNodes)
+            let lightData    = self.getLights(dataNodes)
+            let doc          = dataNodes.document
+            let gridCellSize = dataNodes[doc.children[0]].grid.cellDiameter
+
+            self.updateButtonStatus(game.i18n.localize("DSI.import.parsingfile"), true)
+            let offsetData = await self.parseShapes(geometryData, doorIDs, stairIDs, gridCellSize, offsetX, offsetY)
+            await self.processLights(lightData, offsetData, gridCellSize)
+            self.updateButtonStatus(game.i18n.localize("DSI.import.finished"))
+        } else {
+            $('#dsimportErrorMessage').html(game.i18n.localize("DSI.import.error.noFile")).addClass("visible")
+            self.updateButtonStatus(game.i18n.localize("DSI.import.import"))
+        }
+    }
+
+    activateListeners(html) {
+        super.activateListeners(html)
+
+        html.find("button#processDSFile").click(async (event) => {
+            event.preventDefault()
+            $('#dsimportErrorMessage').html("").removeClass("visible")
+            this.updateButtonStatus(game.i18n.localize("DSI.import.inprogress"), true)
+            let offsetX = $('#dsiImportOffsetX').val()
+            let offsetY = $('#dsiImportOffsetY').val()
+
+            await this.loadFileContent(offsetX.length > 0 ? parseFloat(offsetX) : 0, offsetY.length > 0 ? parseFloat(offsetY) : 0)
+        })
+    }
+}
